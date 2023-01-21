5e6ea3afd813c91b9bc5ef5041c35aeb761249a1
AKINBOBOLA <emmanuelakins908@gmail.com>
2023-01-21 01:50:10 +0100

    added my mongo url

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # secrets.token_hex(20)
 app.config["SECRET_KEY"] = "035aacc41cf96068fc072dbba659d732b5e0994d"
 
-MONGO_URL = "mongodb://localhost:27017"
+MONGO_URL = "mongodb://admin:admin@ac-auxs7ka-shard-00-00.protkog.mongodb.net:27017,ac-auxs7ka-shard-00-01.protkog.mongodb.net:27017,ac-auxs7ka-shard-00-02.protkog.mongodb.net:27017/?ssl=true&replicaSet=atlas-ljpoe4-shard-0&authSource=admin&retryWrites=true&w=majority"
 mongodb_client = MongoClient(MONGO_URL)
 db = mongodb_client["FlaskTodo"]
 todo = db["Todo"]
